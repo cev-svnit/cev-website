2fac42f5f908a7617f890129d296a74d0dd1e0a1
Aastha Soni <aasthasoni1511@gmail.com>
2025-01-26 11:01:43 +0530

    Added Events and Workshops Page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,14 +2,10 @@ import Navbar from "@/components/Navbar";
 
 import Footer from "@/components/Footer";
 
-import LandingPage from "@/components/LandingPage";
-
 export default function Home() {
   return (
     <>
       <Navbar />
-
-      <LandingPage />
 
       <Footer />
 
